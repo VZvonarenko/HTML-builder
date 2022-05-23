ee7700f21caeb5bc5441b884dd229e371b412696
VladimirZ <vzvonarenko@gmail.com>
2022-05-23 10:36:02 +0700

    feat: create 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const folder = path.join(__dirname, '/files');
+const folderCopy = path.join(__dirname, '/files-copy');
+
+fs.stat(folderCopy, err => {
+    if(err) {
+        fs.mkdir(folderCopy, err => {
+            if(err) console.log(err);
+            console.log('Директория files-copy создана');
+             startCopy();
+        });
+    } else {
+        clearDir();
+        startCopy();
+    }
+});
+
+function startCopy() {
+    fs.readdir(folder, (err, files) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Cледующие файлы скопированы в каталог files-copy:');
+            files.forEach(file => {
+                console.log(file);
+                fs.copyFile(folder+ '/' + file, folderCopy + '/' + file, (err) => {
+                    if(err) console.log(err); 
+                });
+            });
+        }
+    });
+}
+
+function clearDir() {
+    fs.readdir(folderCopy, (err, files) => {
+        if(err) {
+            console.log(err);
+        } else {
+            files.forEach(file => {
+                fs.unlink(folderCopy + '/' + file, (err) => {
+                    if (err) console.log(err);
+                });
+            });
+        }
+    });
+}
 
